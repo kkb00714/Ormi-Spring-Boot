09aca1cf2201be4cb14b151e274a0f26eac7fbee
kkb <0furiousblade0@gmail.com>
2024-10-04 10:38:08 +0900

    [#04] Hello Controller - RequestParam 테스트

--- src/main/java/com/example/springdemoproject/controller/HelloController.java
+++ src/main/java/com/example/springdemoproject/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.springdemoproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")  // 패턴 입력
-    public String hello() {
-        return "hello spring";
+    public String hello(@RequestParam(value = "param", defaultValue = " Spring!!") String param) {
+        return "hello" + param;
     }
 }
