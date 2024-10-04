5e7f1863529e9e72a908c8bcf4c9ba0c564f6a4b
kkb <0furiousblade0@gmail.com>
2024-10-04 11:29:48 +0900

    [#05] Hello Service - IOC & DI 테스트

--- src/main/java/com/example/springdemoproject/controller/HelloController.java
+++ src/main/java/com/example/springdemoproject/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.springdemoproject.controller;
 
+import com.example.springdemoproject.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    // DI를 사용해서 spring에게 제어권 맡기기
+    // 필드 선언
+    private final HelloService service;   // Dependency Injection
+
+    public HelloController(HelloService service) {
+        this.service = service;
+    }
 
     @GetMapping("/hello")  // 패턴 입력
     public String hello(@RequestParam(value = "param", defaultValue = " Spring!!") String param) {
-        return "hello" + param;
+        return service.printHello(param);
     }
 }
---
+++ src/main/java/com/example/springdemoproject/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.example.springdemoproject.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class HelloService {
+
+    public String printHello (String param) {
+        return "Hello " + param;
+    }
+}
