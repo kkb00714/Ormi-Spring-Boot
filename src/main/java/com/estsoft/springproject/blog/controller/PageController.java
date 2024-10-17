0a6408aed12ad39fc8b8d9b517129affdfcaa723
kkb <0furiousblade0@gmail.com>
2024-10-17 10:29:25 +0900

    [#37] Person 생성 후 간단한 Thymeleaf 구현

---
+++ src/main/java/com/estsoft/springproject/blog/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.estsoft.springproject.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Controller
+public class PageController {
+    // Person
+    @GetMapping("/thymeleaf/example")
+    public String show(Model model) {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("김자바");
+        person.setAge(23);
+        person.setHobbies(Arrays.asList("게임", "영화보기", "독서", "..."));
+
+        model.addAttribute("person", person);
+        model.addAttribute("today", LocalDateTime.now());
+        // key - value 형태로 person에 접근할 수 있다.
+        return "examplePage"; // html 페이지 리턴
+    }
+}
---
+++ src/main/java/com/estsoft/springproject/blog/controller/Person.java
@@ -0,0 +1,18 @@
+package com.estsoft.springproject.blog.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Person {
+    private Long id;
+    private String name;
+    private int age;
+    private List<String> hobbies;
+}
