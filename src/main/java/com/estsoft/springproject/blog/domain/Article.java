331503f7846c5ba9c8f7fec6e76531d619ebad64
kkb <0furiousblade0@gmail.com>
2024-10-17 16:02:16 +0900

    [#40] Blog Delete Controller & View 생성

--- src/main/java/com/estsoft/springproject/blog/domain/Article.java
+++ src/main/java/com/estsoft/springproject/blog/domain/Article.java
@@ -6,11 +6,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +27,14 @@ public class Article {
 
     @Column(nullable = false)
     private String content;
+
+    @CreatedDate // 생성된 날짜, 시간을 넣어줌
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 업데이트된 날짜, 시간을 넣어줌.
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder // 생성자 생성을 할 때 메서드 체이닝 형식으로 생성해줌
     public Article(String title, String content) {
