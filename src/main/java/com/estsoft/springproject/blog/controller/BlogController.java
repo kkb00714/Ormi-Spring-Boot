ed9c9cf67ea387581f6ba45494d40468264c56eb
kkb <0furiousblade0@gmail.com>
2024-10-22 14:11:14 +0900

    [#55] Spring Controller Api 명세(Apiresponses, Operation, Parameters)

--- src/main/java/com/estsoft/springproject/blog/controller/BlogController.java
+++ src/main/java/com/estsoft/springproject/blog/controller/BlogController.java
@@ -5,7 +5,14 @@ import com.estsoft.springproject.blog.domain.Article;
 import com.estsoft.springproject.blog.domain.dto.ArticleResponse;
 import com.estsoft.springproject.blog.domain.dto.UpdateArticleRequest;
 import com.estsoft.springproject.blog.service.BlogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Value;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +31,10 @@ public class BlogController {
         this.service = service;
     }
 
+    @ApiResponses(value = {     // content - 본문 내용에 있는 컨텐츠를 넣어줄 수 있음
+            @ApiResponse(responseCode = "201", description = "article 생성에 성공했습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Operation(summary = "블로그 글 생성하기", description = "블로그 메인 화면에서 article 생성 페이지로 이동")
     // Create RequestMapping (특정 url   POST /articles)
     @PostMapping("/articles")
     public ResponseEntity<ArticleResponse> writeArticle(
@@ -36,6 +47,13 @@ public class BlogController {
     }
 
     // Read RequestMapping   조회 : GET
+    @ApiResponses(value = {     // content - 본문 내용에 있는 컨텐츠를 넣어줄 수 있음
+            @ApiResponse(responseCode = "200", description = "요청에 성공했습니다", content = @Content(mediaType = "application/json"))
+    })
+    @Operation(summary = "블로그 전체 보기", description = "블로그 메인 화면에서 보여주는 전체 article 목록")
+    @Parameters(value = {
+            @Parameter(name="id", description="블로그 글 ID", example="본문에 들어갈 내용입니다")
+    })
     @GetMapping("/articles")
     public ResponseEntity<List<ArticleResponse>> findArticles() {
         List<Article> articleList = service.findAll();
@@ -45,6 +63,10 @@ public class BlogController {
         return ResponseEntity.ok(list);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "{id}번 article을 조회합니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Operation(summary = "{id}번 블로그 글 조회", description = "{id}번 블로그를 조회합니다.")
     @GetMapping("/articles/{id}")
     public ResponseEntity<ArticleResponse> findOneArticles(@PathVariable Long id) {
         Article article = service.findArticleById(id);
@@ -54,6 +76,7 @@ public class BlogController {
 
     // Delete /articles/{id}
     // @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{id}")
+    @Operation(summary = "블로그 글 삭제", description = "{id}번 블로그를 삭제합니다.")
     @DeleteMapping("/articles/{id}")
     public ResponseEntity<Void> deleteArticles(@PathVariable Long id) {
         service.deleteArticleById(id);
@@ -66,6 +89,10 @@ public class BlogController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());  // 에러 사유를 정의해서 넘겨준다?
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "{id}번 article을 업데이트 합니다.", content = @Content(mediaType = "application/json"))
+    })
+    @Operation(summary = "블로그 글 업데이트", description = "{id}번 article을 업데이트 합니다.")
     // PUT /articles/{id} 수정 API -> RequestBody에 받기
     @PutMapping("/articles/{id}")
     public ResponseEntity<ArticleResponse> updateArticle (
