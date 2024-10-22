bc1baa229c0cddbc49307f39496c458caacd232c
kkb <0furiousblade0@gmail.com>
2024-10-22 14:11:44 +0900

    [#56] Spring Swagger Schema 설정

--- src/main/java/com/estsoft/springproject/blog/domain/dto/ArticleResponse.java
+++ src/main/java/com/estsoft/springproject/blog/domain/dto/ArticleResponse.java
@@ -1,5 +1,6 @@
 package com.estsoft.springproject.blog.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(name = "Article Response DTO", title = "블로그 조회")
 public class ArticleResponse {
+    @Schema(description = "블로그 ID", type = "Long")
     private Long id;
+
+    @Schema(description = "블로그 제목", type = "String")
     private String title;
+
+    @Schema(description = "블로그 sodyd", type = "String")
     private String content;
 }
